c96398695c24227264e390d4a6ae59a5066f8c05
Scott Provence <scott.provence@yahoo.com>
2021-07-17 13:39:08 -0400

    Updat Navbar to reflect Trendz name

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Navbar = ({setProductId}) => {
-  const [newProdId, setNewProdId] = useState('');
+const Navbar = ({ setProductId }) => {
+  const [newProdId, setNewProdId] = useState("");
 
   const searchHandler = () => {
     setProductId(newProdId);
-    setNewProdId('');
+    setNewProdId("");
   };
 
   const changeHandler = (e) => {
@@ -13,23 +13,41 @@ const Navbar = ({setProductId}) => {
   };
 
   const changeColor = () => {
-    if (getComputedStyle(document.documentElement).getPropertyValue('--main-color') === ' rgb(123, 212, 123)') {
-      document.documentElement.style.setProperty('--main-color', 'rgb(58, 54, 54)');
-      document.querySelector('.logo').style.color = 'rgb(255, 255, 255)';
-      document.querySelectorAll('.rp-card, .yo-card').forEach(item => item.style.color = 'rgb(255, 255, 255)');
+    if (
+      getComputedStyle(document.documentElement).getPropertyValue(
+        "--main-color"
+      ) === " rgb(123, 212, 123)"
+    ) {
+      document.documentElement.style.setProperty(
+        "--main-color",
+        "rgb(58, 54, 54)"
+      );
+      document.querySelector(".logo").style.color = "rgb(255, 255, 255)";
+      document
+        .querySelectorAll(".rp-card, .yo-card")
+        .forEach((item) => (item.style.color = "rgb(255, 255, 255)"));
     } else {
-      document.documentElement.style.setProperty('--main-color', ' rgb(123, 212, 123)');
-      document.querySelector('.logo').style.color = 'rgb(0, 0, 0)';
-      document.querySelectorAll('.rp-card, .yo-card').forEach(item => item.style.color = 'rgb(0, 0, 0)');
+      document.documentElement.style.setProperty(
+        "--main-color",
+        " rgb(123, 212, 123)"
+      );
+      document.querySelector(".logo").style.color = "rgb(0, 0, 0)";
+      document
+        .querySelectorAll(".rp-card, .yo-card")
+        .forEach((item) => (item.style.color = "rgb(0, 0, 0)"));
     }
   };
 
   return (
     <nav className="navbar">
-      <div className="logo" onClick={changeColor}>LOGO</div>
+      <div className="logo" onClick={changeColor}>
+        TRENDZ
+      </div>
       <div className="search-area">
         <input type="text" value={newProdId} onChange={changeHandler} />
-        <button className="search-button" onClick={searchHandler} >Search</button>
+        <button className="search-button" onClick={searchHandler}>
+          Search
+        </button>
       </div>
     </nav>
   );
